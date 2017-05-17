178bc1c2813a82e7393b2f0b622ce6b070b0e17e
jackgullen <1401294@abertay.ac.uk>
2017-05-17 13:13:58 +0100

    Added color changine characters

---
+++ Assets/Scipts/colourChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class colourChanger : MonoBehaviour {
+
+	public Image characterImage;
+	private float startTime;
+
+	private Color blue;
+	private Color red;
+
+	// Use this for initialization
+	void Start () 
+	{
+		startTime = Time.time;
+
+		blue = new Color32 (0, 66, 124, 255);
+		red = new Color32 (193, 3, 3, 255);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ((Time.time - startTime) > 0 && (Time.time - startTime) < 5)
+		{
+			characterImage.color = Color.white;
+		}
+		else if ((Time.time - startTime) > 5 && (Time.time - startTime) < 10)
+		{
+			characterImage.color = blue;
+		}
+		else if ((Time.time - startTime) > 10)
+		{
+			characterImage.color = red;
+		}
+	}
+}
